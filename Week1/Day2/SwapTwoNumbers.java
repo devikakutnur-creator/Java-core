b6146ec9c3f7663b456cdd1f837e90b48038d023
devikakutnur-creator <devikakutnur@gmail.com>
2026-01-02 21:17:41 +0530

    Add Week 1 Day 2 Swap two numbers program

---
+++ Week1/Day2/SwapTwoNumbers.java
@@ -0,0 +1,10 @@
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
