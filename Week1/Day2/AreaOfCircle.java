da8cd8d18bc53f1a37d3e9f18d2657a62c9debf1
devikakutnur-creator <devikakutnur@gmail.com>
2026-01-02 21:23:35 +0530

    Add Week 1 Day 2 Area of circle program

---
+++ Week1/Day2/AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 5;   
+        double pi = 3.14159; 
+        double area = pi * radius * radius;
+
+        System.out.println("Radius = " + radius);
+        System.out.println("Area of the circle = " + area);
+    }
+}
