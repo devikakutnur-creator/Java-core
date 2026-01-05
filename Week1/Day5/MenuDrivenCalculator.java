734dfa7ab3b4442258afeb8a499911171129e2fc
KutnurDevika <devikakutnur@gmail.com>
2026-01-05 15:05:41 +0530

    Add Week 1 Day 5 menu driven calculator program

---
+++ Week1/Day5/MenuDrivenCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class MenuDrivenCalculator {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+        System.out.print("Enter your choice: ");
+
+        int choice = sc.nextInt();
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + (a + b));
+                break;
+            case 2:
+                System.out.println("Result: " + (a - b));
+                break;
+            case 3:
+                System.out.println("Result: " + (a * b));
+                break;
+            case 4:
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Cannot divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+sc.close();
+    }
+}
