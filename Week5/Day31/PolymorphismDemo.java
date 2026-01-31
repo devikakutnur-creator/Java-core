a2d710257b9f818c5a73d8b0f217e639c2221989
KutnurDevika <devikakutnur@gmail.com>
2026-01-31 13:17:19 +0530

    Create PolymorphismDemo.java

---
+++ Week5/Day31/PolymorphismDemo.java
@@ -0,0 +1,38 @@
+class Calculator {
+
+    // Compile-time polymorphism (method overloading)
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+
+        // Compile-time polymorphism
+        Calculator calc = new Calculator();
+        System.out.println("Sum of 2 numbers: " + calc.add(10, 20));
+        System.out.println("Sum of 3 numbers: " + calc.add(10, 20, 30));
+
+        // Runtime polymorphism
+        Animal a = new Dog();
+        a.sound();
+    }
+}
